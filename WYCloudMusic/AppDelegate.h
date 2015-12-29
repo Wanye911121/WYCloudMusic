867f595bea7308d81b550dfbe85d6cbd335cfac7
MikeZhangpy <zhangpy1991@126.com>
2015-12-29 09:44:07 +0800

    Initial Commit

---
+++ WYCloudMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYCloudMusic
+//
+//  Created by ZpyZp on 15/12/29.
+//  Copyright © 2015年 zpy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
